60e783f1bb1073fa677d45f864167b6319135680
Gladys Rodriguez <gla.rodriguez.sanchez@gmail.com>
2021-03-02 21:55:10 -0600

    Integración de banner en pagina de inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,4 +5,27 @@
 @section('content')
 <!-- Aquí va contenido -->
 <!-- Prueba 1-->
+<div class="bg--blanco">
+    <!-- Inicio Banner -->
+    <div class="container  ">
+    <div class="bloque--conBorde">
+
+    <div class="container  ">
+    <div class="text-center">
+
+    <img src="{{asset('img/cecyt13/banner.png')}}" alt="" class="img-fluid">
+
+    </div>
+    </div>
+
+    </div>
+    </div>
+    <!-- Fin Banner -->
+
+    <!-- Inicio Carrusel -->
+
+    <!-- Fin Carrusel -->
+
+</div>
+
 @endsection
