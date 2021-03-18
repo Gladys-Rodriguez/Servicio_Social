02580d909982d2032e320a1edbea6b7d4193ab01
Gladys Rodriguez <gla.rodriguez.sanchez@gmail.com>
2021-03-17 19:19:13 -0600

    Creación de formulario de acceso

--- resources/views/Servicio_Inicio.blade.php
+++ resources/views/Servicio_Inicio.blade.php
@@ -2,6 +2,25 @@
 
 @section('title', 'Servicio')
 
+
 @section('content')
+
 <!-- Aquí va contenido -->
+
+
+        <!-- Metodo Get sin validar nada -->
+        <form class="box_acceso" action="{{ url('/Servicio') }}" method="GET">
+            @csrf
+            <h1> Acceso </h1>
+            <input type="text" name="" placeholder="No. De Boleta">
+            <input type="text" name="" placeholder="No. De Registro">
+        <input type="submit" name="" value="Ingresar">
+        </form>
+
+
+
+<!-- Fin contenido -->
 @endsection
+
+
+
