15e1c4d9b461b562fa00d3844696043ee2c1f6a7
Gladys Rodriguez <gla.rodriguez.sanchez@gmail.com>
2021-03-17 19:20:05 -0600

    Creacion de ruta get a los datos y comentar rutas post

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,22 @@ Route::get('/', function () {
     return view('home');
 }) -> name('home');
 
-Route::get('/Servicio_Social_Datos', function (){
+/*Route::get('/Servicio_Social/Datos', function (){
     return view('Servicio');
-}) -> name('Servicio');
+}) -> name('Servicio');*/
+
+//Route::post('/Servicio', 'Datos@save');
 
 Route::get('/Servicio_Social', function (){
     return view('Servicio_Inicio');
 }) -> name('Servicio_Inicio');
 
+Route::get('/Servicio', function (){
+    return view('Servicio');
+}) -> name('Servicio');
+
 Auth::routes();
+
+//Route::get('/Servicio', [App\Http\Controllers\Datos::class, 'create'])->name('Servicio');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
