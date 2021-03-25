cb07db1900abec7fc3e48a97f4e033e1cb2e2594
Gladys Rodriguez <gla.rodriguez.sanchez@gmail.com>
2021-03-25 03:12:06 -0600

    Migracion, modelo y controlador de Direcciones

---
+++ database/migrations/2021_03_25_080459_direcciones.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Direcciones extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('Direcciones', function(Blueprint $table){
+            $table->increments('Id_direccion');
+            $table->string('Alcaldia');
+            $table->string('Colonia');
+            $table->string('Calle');
+            $table->string('Num_ext');
+            $table->string('Num_int')->nullable();
+            $table->integer('CP');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('Direcciones');
+    }
+}
