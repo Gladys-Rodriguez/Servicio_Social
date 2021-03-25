9877d5077b2f7b3de31c2fdc01075fe8f198df9e
Gladys Rodriguez <gla.rodriguez.sanchez@gmail.com>
2021-03-25 03:10:44 -0600

    Migracion, modelo y controlador de Datos_Dependencia

---
+++ app/Models/Dato_Dependencia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dato_Dependencia extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_03_25_082814_datos__dependencias.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DatosDependencias extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create ('Datos_Dependencias', function(Blueprint $table){
+            $table->integer('Clave_Dependencia')->notnull();
+            $table->string('Nombre_Dependencia', 30);
+            $table->string('Nom_Responsable', 30);
+            $table->string('Ap_Responsable', 30);
+            $table->string('Am_Responsable', 30);
+            $table->string('Telefono', 30);
+            $table->string('email')->unique();
+            $table->integer('Direc_Depen')->unsigned();
+
+            $table->primary('Clave_Dependencia');
+            $table->foreign('Direc_Depen')->references('Id_direccion')->on('Direcciones')->onDelete('cascade');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('Datos_Dependencias');
+    }
+}
