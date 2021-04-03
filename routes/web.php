75199ae77c5d9bfaa84cb55adc65689f56d8d1a7
Ivan Arteaga Torres <ivanarteagatorres8@gmail.com>
2021-04-02 19:23:48 -0600

    Avance de pantallas generales y login

--- routes/web.php
+++ routes/web.php
@@ -40,8 +40,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 // Nuevas rutas
 
 Route::get('/', function () {
-    return view('Inicio');
-}) -> name('Inicio');
+    return view('Pantallas_Principales.Index_gnral');
+}) -> name('Index_gnral');
+
 
 //Ruta para Formatos Administrativos de la diapositiva 12
 Route::get('/FormatosAdministrativos', function (){
@@ -120,7 +121,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/files', [App\Http\Controllers\FilesController::class, 'index'])->name('index');
 Route::post('/upload', [App\Http\Controllers\FilesController::class, 'store'])->name('user.files.store');
 
-//------------------------IVAN------------------------------------
+//------------------------IVAN INICIO------------------------------------
 
 //Pantalla general
 Route::get('/Index_gnral', function (){
@@ -148,16 +149,29 @@ Route::get('/PracticasIndex', function (){
     return view('Pantallas_Principales.PracticasIndex');
 }) -> name('PracticasIndex');
 
-//Pantalla Login
-Route::get('/LoginForm', function (){
-    return view('Pantallas_Principales.LoginForm');
-}) -> name('LoginForm');
-
 //Pantalla Registro
 Route::get('/RegisterForm', function (){
     return view('Pantallas_Principales.RegisterForm');
 }) -> name('RegisterForm');
 
-//------------------------IVAN------------------------------------
+//RUTAS PARA EL LOGIN
+//Pantalla Login
+Route::get('/LoginForm', function (){
+    return view('Pantallas_Principales.LoginForm');
+}) -> name('LoginForm');
+
+Route::post('LoginForm',function(){
+    return 'Post login';
+});
+
+//------------------------IVAN TERMINO------------------------------------
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
