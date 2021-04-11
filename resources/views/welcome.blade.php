f5d298f08f6fa4802762c5b7efc6da057972511d
Ivan Arteaga Torres <ivanarteagatorres8@gmail.com>
2021-04-10 20:02:20 -0500

    NO USEN ESTA RAMA POR NADA DEL MUNDO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         Usuario Logueado
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
