f91a539bc51b23a82b71dbcd56e3823c79b686d9
santos123 <jsantosss81@gmail.com>
2021-05-04 00:22:50 -0500

    Primer avance admin prácticas

--- resources/views/Pantallas_Admin_Practicas_Visitas/Clasificacion_Practicas_Visitas.blade.php
+++ resources/views/Pantallas_Admin_Practicas_Visitas/Clasificacion_Practicas_Visitas.blade.php
@@ -1,89 +1,58 @@
 @extends('Pantallas_Admin_Practicas_Visitas.navbar_Clasificacion_Practicas_Visitas')
 
 @section('css')
-<link rel="stylesheet" href="{{asset('css/Pantallas_Admin_PracticasVisitas/estilo.css')}}">
+<link rel="stylesheet" href="{{asset('css/Pantallas_Admin_PracticasVisitas/estilosSolicitudes.css')}}">
 @endsection
 
 @section('content')
-        <div class="container">
-            <div class="card">
-                <div class=imgBx>
-                    <img src="{{asset('img/valpracticas/calendario.jpg')}}" alt="">
-                    </div>
-                    <div class="contentBx">
-                        <div class="content">
-                        <p><a href="{{asset('https://www.ipn.mx/assets/files/main/docs/inicio/cal-Escolarizada-20-21.pdf')}}" target="_blank"> <h1> Calendario de visitas </h1> </a> </p>
-                    
-                    </div>
-                </div>
-            </div>
+<input type="checkbox" id="btn-modal">
+	<div class="container">
+			<table class="table">
+					<caption>Consolas</caption>
+					<thead>
+							<tr>
+									<th>Consola</th>
+									<th>Precio</th>
+									<th>Ventas</th>
+									<th>Fecha de lanzamiento</th>
+									<th></th>
+							</tr>
+					</thead>
+					<tbody>
+							<tr>
+									<td data-label="Consola">Play Station 3</td>
+									<td data-label="Precio">$8000</td>
+									<td data-label="Ventas">800000</td>
+									<td data-label="Fecha de lanzamiento">10/01/2012</td>
+									<td></td>
+							</tr>
+							<tr>
+									<td data-label="Consola">Play Station 3</td>
+									<td data-label="Precio">$8000</td>
+									<td data-label="Ventas">800000</td>
+									<td data-label="Fecha de lanzamiento">10/01/2012</td>
+									<td></td>
+							</tr>
+							<tr>
+									<td data-label="Consola">Play Station 3</td>
+									<td data-label="Precio">$8000</td>
+									<td data-label="Ventas">800000</td>
+									<td data-label="Fecha de lanzamiento">10/01/2012</td>
+									<td><label for="btn-modal" class="lbl-modal">Ver Más</label></td>
+							</tr>
+					</tbody>
+			</table>
+	</div>
 
-            <div class="card">
-                <div class=imgBx>
-                    <img src="{{asset('img/valpracticas/requisitos.jpg')}}" alt="">
-                    </div>
-                    <div class="contentBx">
-                        <div class="content">
-                        <p>  <a href="{{asset('https://www.ipn.mx/assets/files/secgeneral/docs/Memorias/MemoriaAnual2016/NMS/CECyT13.pdf')}}" target="_blank"> <h1> Requerimientos de empresas </h1> </a></p>
-                    
-                    </div>
-                </div>
-            </div>
+	<div class="modal">
+		<div class="contenedor">
+			<header>¡Bienvenidos!</header>
+			<label for="btn-modal">X</label>
+			<div class="contenido">
+				<h3><!-- Agregar un titulo --></h3>
+				<p><!-- Agregar un mensaje --></p>
+			</div>
+		</div>
+	</div>
 
-
-            <div class="card">
-                <div class=imgBx>
-                    <img src="{{asset('img/valpracticas/solicitud.jpg')}}" alt="">
-                    </div>
-                    <div class="contentBx">
-                        <div class="content">
-                        <p><a href="{{asset('https://www.cecyt13.ipn.mx/assets/files/cecyt13/docs/conocenos/certificacion/19PRAVIS.pdf')}}" target="_blank"> <h1> Solicitud de visita </h1> </a></p>
-                    
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="card">
-                <div class=imgBx>
-                    <img src="{{asset('img/valpracticas/informe1.jpg')}}" alt="">
-                    </div>
-                    <div class="contentBx">
-                        <div class="content">
-                        <p> <a href="{{asset('https://www.cecyt13.ipn.mx/assets/files/cecyt13/docs/conocenos/certificacion/18CONDOCEXT.pdf')}}" target="_blank"> <h1> Informe de visita </h1> </a> </p>
-                    
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="card">
-                <div class=imgBx>
-                    <img src="{{asset('img/valpracticas/informe2.jpg')}}" alt="">
-                    </div>
-                    <div class="contentBx">
-                        <div class="content">
-                        <p> <a href="{{asset('https://www.cecyt13.ipn.mx/assets/files/cecyt13/docs/conocenos/certificacion/18CONDOCEXT.pdf')}}" target="_blank"> <h1> Informe de visita concluida </h1> </a></p>
-                    
-                    </div>
-                </div>
-            </div>
-
-            
-            <div class="card">
-                <div class=imgBx>
-                    <img src="{{asset('img/valpracticas/comprobante.png')}}" alt="">
-                    </div>
-                    <div class="contentBx">
-                        <div class="content">
-                        <p> <a href="{{asset('https://www.cecyt13.ipn.mx/assets/files/cecyt13/docs/conocenos/certificacion/19PRAVIS.pdf')}}" target="_blank"> <h1> Documentos comprobatorios </h1> </p>
-                    
-                    </div>
-                </div>
-            </div>
-
-          
-
-
-        </div>
         @endsection
