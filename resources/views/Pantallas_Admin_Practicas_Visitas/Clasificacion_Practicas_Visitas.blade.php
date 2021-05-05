0fd1b7081fdef613920746c56f4032170f43de95
Fernando_alejandre <falejandre11@hotmail.com>
2021-05-05 17:38:49 -0500

    Se agrego la ventana "Solicitudes de Practicas" en el modulo Admin de Practicas y Visitas

--- resources/views/Pantallas_Admin_Practicas_Visitas/Clasificacion_Practicas_Visitas.blade.php
+++ resources/views/Pantallas_Admin_Practicas_Visitas/Clasificacion_Practicas_Visitas.blade.php
@@ -22,10 +22,10 @@
 					<tbody>
 							<tr>
 									<td data-label="Id Solicitante">1</td>
-									<td data-label="Solicitante">Joel Peña Huitrón</td>
+									<td data-label="Solicitante">Regina Alejandre Sánchez</td>
 									<td data-label="Empresa">Bimbo</td>
 									<td data-label="Grupo">2CM20</td>
-									<td data-label="Fecha de visita">11/05/2021</td>
+									<td data-label="Fecha de visita">19/05/2021</td>
 									<td><label for="btn-modal" class="lbl-modal">Ver Más</label></td>
 							</tr>
 					</tbody>
@@ -38,14 +38,14 @@
 			<label for="btn-modal">X</label>
 			<div class="contenido">
 				<h3>Solicitud No:1</h3>
-				<p><strong>Solicitante:</strong>  Joel Peña Huitrón <br>
+				<p><strong>Solicitante:</strong>  Regina Alejandre Sánchez <br>
 				<strong>Grupo:</strong> 2CM20 <br>
 				<strong>Número de alumnos:</strong> 50 <br>
 				<strong>Empresa:</strong> Bimbo <br>
-				<strong>Dirección:</strong> Mimosas No. 117 Col Sta. María Insurgentes Delegación Cuauhtémoc, C.P. 06430 Ciudad de México <br>
-				<strong>Fecha de visita:</strong> 11/05/2021 <br>
+				<strong>Dirección:</strong> Malacates No. 15 Col Sta. María Insurgentes Delegación Cuauhtémoc, C.P. 06430 Ciudad de México <br>
+				<strong>Fecha de visita:</strong> 19/05/2021 <br>
 				<strong>Documentos: </strong></p>
-				<a href="#" class="label_a">Permiso</a> <a href="#" class="label_a">Informe</a> <br>
+				<a href="#" class="label_a">Solicitud de Visita</a>
 				<hr><hr><hr><br><br><br>
 				<a href="#" class="label_c">Rechazar</a> <a href="#" class="label_b">Aceptar</a> <br>
 			</div>
