008084e5e5164b631d00ab8f7022985249b98f64
Ivan Arteaga Torres <ivanarteagatorres8@gmail.com>
2021-05-07 22:38:14 -0500

    consulta general lista

--- resources/views/Pantallas_Principales/consultaPrueba.blade.php
+++ resources/views/Pantallas_Principales/consultaPrueba.blade.php
@@ -17,12 +17,29 @@
     <div class="tabla-consulta">
          <table border="1">
              <tr>
-                 <td></td>
-                 <td>Boleta</td>
 
+                 <td>Matricula</td>
                  <td>Correo</td>
-                 <td>Id_rol</td>
-                 <td>Tipo de Rol</td>
+
+                 <td>Nombre</td>
+                 <td>Apellido paterno</td>
+                 <td>Apellido materno</td>
+                 <td>Telefono</td>
+                 <td>Celular</td>
+
+                 <td>Ciudad</td>
+                 <td>Alcaldia</td>
+                 <td>Colonia</td>
+                 <td>Calle</td>
+                 <td>Numero ext</td>
+                 <td>Numero int</td>
+                 <td>Codigo postal</td>
+
+                 <td>Carrera</td>
+                 <td>Semestre</td>
+                 <td>Grupo</td>
+                 <td>Turno</td>
+
             </tr>
                @foreach($Users as $User)
                <tr>
@@ -30,8 +47,25 @@
                  <a class="boton_personalizado" href="{{url('showPrueba', $User->id)}}">Ver</a>
                  <td>{{$User->id}}</td>
                  <td>{{$User->email}}</td>
-                 <td>{{$User->id_rol}}</td>
-                 <td>{{$User->Tipo_rol}}</td>
+
+                 <td>{{$User->nombre}}</td>
+                 <td>{{$User->ap_paterno}}</td>
+                 <td>{{$User->ap_materno}}</td>
+                 <td>{{$User->telefono}}</td>
+                 <td>{{$User->celular}}</td>
+
+                 <td>{{$User->ciudad}}</td>
+                 <td>{{$User->alcaldia}}</td>
+                 <td>{{$User->colonia}}</td>
+                 <td>{{$User->calle}}</td>
+                 <td>{{$User->num_ext}}</td>
+                 <td>{{$User->num_int}}</td>
+                 <td>{{$User->cp}}</td>
+
+                 <td>{{$User->carrera}}</td>
+                 <td>{{$User->semestre}}</td>
+                 <td>{{$User->grupo}}</td>
+                 <td>{{$User->turno}}</td>
                 <tr>
                @endforeach
         </table>
