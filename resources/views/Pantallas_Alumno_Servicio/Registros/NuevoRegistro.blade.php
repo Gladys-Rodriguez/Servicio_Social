632c18f113c96e01c9a332a27793ec0e961df791
Ivan Arteaga Torres <ivanarteagatorres8@gmail.com>
2021-05-14 20:32:34 -0500

    Validaciones en el formulario de registro general y en el formulario de servicio

--- resources/views/Pantallas_Alumno_Servicio/Registros/NuevoRegistro.blade.php
+++ resources/views/Pantallas_Alumno_Servicio/Registros/NuevoRegistro.blade.php
@@ -11,6 +11,57 @@
 @endsection
 
 @section('content')
+<script>
+    function solonumeros(e){
+        key=e.keyCode||e.wich;
+
+        teclado=String.fromCharCode(key);
+
+        numeros="0123456789";
+
+        especiales="8-37-38-46";
+
+        teclado_especial=false;
+
+        for(var i in especiales){
+            if(key==especiales[i]){
+                teclado_especial=true;
+            }
+        }
+
+        if(numeros.indexOf(teclado)==-1 && !teclado_especial){
+            return false;
+        }
+
+    }
+    </script>
+
+
+<script>
+    function sololetras(e){
+        key=e.keyCode||e.wich;
+
+        teclado=String.fromCharCode(key);
+
+        letras="abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+        especiales="8-37-38-46";
+
+        teclado_especial=false;
+
+        for(var i in especiales){
+            if(key==especiales[i]){
+                teclado_especial=true;
+            }
+        }
+
+        if(letras.indexOf(teclado)==-1 && !teclado_especial){
+            return false;
+        }
+
+    }
+    </script>
+
 <div class="container">
 
     <div class="progress-bar">
@@ -57,31 +108,31 @@
                     <br>
                         <div class="field">
                             <div class="label" for="ciudad">Ciudad:</div>
-                            <input type="text" name="ciudad" id="ciudad">
+                            <input type="text" name="ciudad" id="ciudad" placeholder="Ingresa la ciudad" required maxlength="60" onkeypress="return sololetras(event)">
                         </div>
                         <div class="field">
-                            <div class="label" for="alcaldia">Alcaldia:</div>
-                            <input type="text" name="alcaldia" id="alcaldia">
+                            <div class="label" for="alcaldia">Alcaldia o Municipio:</div>
+                            <input type="text" name="alcaldia" id="alcaldia" placeholder="Ingresa la alcaldia o municipio" required maxlength="60" onkeypress="return sololetras(event)">
                         </div>
                         <div class="field">
                             <div class="label" for="colonia">Colonia:</div>
-                            <input type="text" name="colonia" id="colonia">
+                            <input type="text" name="colonia" id="colonia" placeholder="Ingresa la colonia" required maxlength="60" onkeypress="return sololetras(event)">
                         </div>
                         <div class="field">
                             <div class="label" for="calle">Calle:</div>
-                            <input type="text" name="calle" id="calle">
+                            <input type="text" name="calle" id="calle" placeholder="Ingresa la calle" required maxlength="60">
                         </div>
                         <div class="field">
                             <div class="label" for="num_ext">Numero Ext.:</div>
-                            <input type="text" name="num_ext" id="num_ext">
+                            <input type="text" name="num_ext" id="num_ext" placeholder="Ingresa el numero exterior" required maxlength="5" onkeypress="return solonumeros(event)">
                         </div>
                         <div class="field">
                             <div class="label" for="num_int">Numero Int.:</div>
-                            <input type="text" name="num_int" id="num_int">
+                            <input type="text" name="num_int" id="num_int" placeholder="Ingresa el numero interior" required maxlength="5" onkeypress="return solonumeros(event)">
                         </div>
                         <div class="field">
                             <div class="label" for="cp">CP:</div>
-                            <input type="Number" name="cp" id="cp">
+                            <input type="text" name="cp" id="cp" placeholder="Ingresa el codigo postal" required maxlength="5" onkeypress="return solonumeros(event)">
                         </div>
                         <div class="field">
                             <button class="firstNext next">Next</button>
@@ -95,7 +146,7 @@
 
                         <div class="field">
                             <div class="label" for="nombre_depen">Nombre de la Dependencia:</div>
-                            <input type="text" name="nombre_depen" id="nombre_depen">
+                            <input type="text" name="nombre_depen" id="nombre_depen" placeholder="Ingresa la dependencia" required maxlength="60" onkeypress="return sololetras(event)">
                         </div>
 
                         <div class="field btns">
@@ -110,23 +161,23 @@
                     <br>
                     <div class="field">
                         <div class="label" for="nom_responsable">Nombre:</div>
-                        <input type="text" name="nom_responsable" id="nom_responsable">
+                        <input type="text" name="nom_responsable" id="nom_responsable" placeholder="Ingresa el nombre completo" maxlength="60" onkeypress="return sololetras(event)">
                     </div>
                     <div class="field">
                         <div class="label" for="ap_responsable">Apellido Paterno:</div>
-                        <input type="text" name="ap_responsable" id="ap_responsable">
+                        <input type="text" name="ap_responsable" id="ap_responsable" placeholder="Ingresa el apellido paterno" maxlength="60" onkeypress="return sololetras(event)">
                     </div>
                     <div class="field">
                         <div class="label" for="am_responsable">Apellido Materno:</div>
-                        <input type="text" name="am_responsable" id="am_responsable">
+                        <input type="text" name="am_responsable" id="am_responsable" placeholder="Ingresa el apellido materno" maxlength="60" onkeypress="return sololetras(event)">
                     </div>
                     <div class="field">
                         <div class="label" for="telefono">Telefono:</div>
-                        <input type="Number" name="telefono" id="telefono">
+                        <input type="text" name="telefono" id="telefono" placeholder="Ingresa el telefono" required minlength="10" maxlength="10" onkeypress="return solonumeros(event)">
                     </div>
                     <div class="field">
                         <div class="label" for="email_responsable">E-mail:</div>
-                        <input type="text" name="email_responsable" id="email_responsable">
+                        <input type="text" name="email_responsable" id="email_responsable" placeholder="Ingresa el correo electronico" required pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}">
                     </div>
                         <div class="field btns">
                             <button class="prev-2 prev">Previous</button>
@@ -140,7 +191,7 @@
                     <br>
                          <div class="field">
                             <div class="label" for="No_registro">No. de Registro:</div>
-                            <input type="No_registro" name="No_registro" id="No_registro">
+                            <input type="No_registro" name="No_registro" id="No_registro" placeholder="Ingresa el numero de registro" required maxlength="20" onkeypress="return solonumeros(event)">
                         </div>
                         <div class="field">
                             <div class="label" for="fecha_inicio">Fecha de Inicio:</div>
