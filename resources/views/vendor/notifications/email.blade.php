558350dde321c482e2c63cbb1aba9bfc3de78487
Ivan Arteaga Torres <ivanarteagatorres8@gmail.com>
2021-06-19 17:27:24 -0500

    Modificacion en parte del correo de confirmación

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -51,8 +51,8 @@
 @isset($actionText)
 @slot('subcopy')
 @lang(
-    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
+    "Si tiene problemas para hacer clic en el botón \":actionText\", copie y pegue la siguiente URL\n".
+    'en su navegador web:',
     [
         'actionText' => $actionText,
     ]
