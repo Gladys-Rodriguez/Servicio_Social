d8e99556997c7a8ce0c892b1dd76ffb070f06ba5
Gladys Rodriguez <gla.rodriguez.sanchez@gmail.com>
2021-07-01 14:12:31 -0500

    archivos no funcionales

---
+++ resources/views/Pantallas_Alumno_Servicio/Index_AlumnoV2.blade.php
@@ -0,0 +1,75 @@
+@extends('Pantallas_Alumno_Servicio.Layout_Alumno.Layout_Alumno4')
+
+@section('title', 'Registro')
+
+@section('css')
+
+
+<link rel="stylesheet" href="{{asset('css/Pantallas_Alumno_Servicio/Index_Alumno.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+@endsection
+
+@section('content')
+<main>
+<section class="container">
+    <div class="site-content">
+        <div class="posts">
+
+            <div class="post-content" data-aos="zoom-in" data-aos-delay="200">
+                <div class="post-image">
+                    <div>
+                        <img src="{{asset('img/Alumno/SS.jpg')}}" class="img" alt="blog1">
+                    </div>
+                    <!--<div class="post-info flex-row">
+                        <span><i class="fas fa-user text-gray"></i>&nbsp;&nbsp;Admin</span>
+                        <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 14, 2019</span>
+                        <span>2 Commets</span>
+                    </div>-->
+                </div>
+                <div class="post-title">
+                    <a href="#">¿Qué es el Servicio Social? </a>
+                    <p>
+                    Es una actividad formativa que amplia tu información académica
+                    y fomenta una conciencia de solidaridad con la sociedad.
+                    Es de carácter temporal y obligatorio además de ser un requisito
+                    indispensable para la titulación, que te permite obtener competencia laboral.
+                    .. Prueba para commit
+                    </p>
+
+                </div>
+            </div>
+
+            <hr>
+        </div>
+
+        <aside class="sidebar">
+            <div class="category">
+                <h2>---------</h2>
+                <ul class="category-list">
+                    <li class="list-items" data-aos="fade-left" data-aos-delay="100">
+                        <a href="{{url('/NuevoRegistro')}}" id="new_register">Nuevo Registro</a>
+                        <span><i class="fas fa-user text-gray"></i></span>
+                    </li>
+                    <li class="list-items" data-aos="fade-left" data-aos-delay="200">
+                        <a href="#">Descargar Formatos</a>
+                        <span><i class="fas fa-user text-gray"></i></span>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+
+    </div>
+</section>
+</main>
+
+
+@endsection
+
+@section('script')
+<script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"> </script>
+
+<script src="{{asset('js/Pantallas_Principales/Index_Alumno.js')}}"></script>}
+<script src="{{asset('js/Pantallas_Alumno_Servicio/Registros/NuevoRegistro.js')}}"></script>
+
+
+@endsection
---
+++ resources/views/Pantallas_Alumno_Servicio/Layout_Alumno/Layout_Alumno4.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{asset('css/Pantallas_Alumno_Servicio/Layout_Alumno/Layout_Alumno2.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    @yield('css')
+
+  </head>
+  <body>
+
+    <header class="header">
+      <a href="#" class="brand">APOYOS EDUCATIVOS</a>
+      <a href="#" class="header__logo"></a>
+        <i class='bx bx-menu header__toggle' id="header-toggle"></i>
+      <nav class="nav-nav" id="nav-menu">
+        <div class="nav__content bd-grid">
+            <a href="" class="nav__perfil">
+                <div class="nav__img">
+                    <img src="assets/img/perfil.jpg" alt="">
+                </div>
+
+                <div>
+                   <!-- <span class="nav__name">Clay</span>
+                    <span class="nav__name">Mitchell</span>
+                </div> -->
+            </a>
+
+            <div class="nav__menu">
+                <ul class="nav__list">
+                    <li class="nav__item"><a href="{{ url('/Index_Alumno')}}" class="nav__link active">Home</a></li>
+
+                    <li class="nav__item dropdown">
+                        <a href="/Index_Alumno" class="nav__link dropdown__link">Servicio Social <i class='bx bx-chevron-down dropdown__icon'></i></a>
+                        <ul class="dropdown__menu">
+                            <li class="dropdown__item"><a href="{{ url('/docexpediente')}}" class="nav__link">Subir documentos</a></li>
+                            <li class="dropdown__item"><a href="{{url('/docsSolicitudAlumno')}}" class="nav__link">Seguimiento de solicitud</a></li>
+                           <!-- <li class="dropdown__item"><a href="#" class="nav__link">Location</a></li>
+                            <li class="dropdown__item"><a href="#" class="nav__link">Studies</a></li> -->
+                        </ul>
+                    </li>
+
+                    <!--<li class="nav__item"><a href="#" class="nav__link">Skills</a></li>-->
+
+                    <li class="nav__item dropdown">
+                        <a href="#" class="nav__link dropdown__link">Becas <i class='bx bx-chevron-down dropdown__icon'></i></a>
+
+
+                        <ul class="dropdown__menu">
+                            <li class="dropdown__item"><a href="#" class="nav__link">Convocatoria</a></li>
+
+                            <li class="dropdown__item"><a href="#" class="nav__link">Registro</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav__item"><a href="{{ url('/DatosPersonales')}}" class="nav__link">Datos personales</a></li>
+
+                    <li class="nav__item"><a href="{{ url('/logout')}}" class="nav__link"> Cerrar Sesión</a></li>
+
+                   <!-- <li class="nav__item"><a href="#" class="nav__link">Contact</a></li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    </header>
+    <script src="{{asset('js/Pantallas_Alumno_Servicio/Layout_Alumno.js')}}"></script>
+    <script type="text/javascript">
+        //javascript for navigation bar effect on scroll
+        window.addEventListener("scroll", function(){
+          var header = document.querySelector("header");
+          header.classList.toggle('sticky', window.scrollY > 0);
+        });
+
+        //javascript for responsive navigation sidebar menu
+        var menu = document.querySelector('.menu');
+        var menuBtn = document.querySelector('.menu-btn');
+        var closeBtn = document.querySelector('.close-btn');
+
+        menuBtn.addEventListener("click", () => {
+          menu.classList.add('active');
+        });
+
+        closeBtn.addEventListener("click", () => {
+          menu.classList.remove('active');
+        });
+
+
+        $(document).ready(function(){
+              //jquery for toggle sub menus
+              $('.sub-btn').click(function(){
+                $(this).next('.sub-menu').slideToggle();
+                $(this).find('.dropdown').toggleClass('rotate');
+              });
+            });
+
+        </script>
+
+<!--    <section class="section-main">
+      <h1>Datos personales</h1>
+    </section>-->
+    <section class="section-two">
+ <!-- AQUI EMPIEZA CONTENIDO; -->
+      @yield('content')
+
+    </section>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+
+
+
+    @yield('script')
+
+  </body>
+</html>
