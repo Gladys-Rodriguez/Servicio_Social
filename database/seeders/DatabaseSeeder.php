db0ec41359a93586acfacdc69bb8db2beb5d71c4
DESKTOP-RIRTQC6\MoisesErick <moises.erick0598@gmail.com>
2021-08-07 11:21:14 -0500

    Eliminación de Seeder que ya no se ocupan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call(RolSeeder::class);
-       // $this->call(VisitaEstadosSeeder::class);
-        //$this->call(UserSeeder::class);
-        //$this->call(DatoSeeder::class);
-        //$this->call(DocenteSeeder::class);
         //$this->call(CarreraSeeder::class);
-        //$this->call(DireccionSeeder::class);
-        //$this->call(EmpresaSeeder::class);
         //$this->call(GrupoSeeder::class);
-        //$this->call(VisitaSeeder::class);
-        //$this->call(TipoDocumentoSeeder::class);
-        //$this->call(VisitaDocumentoSeeder::class);
-        //$this->call(GrupoVisitaSeeder::class);
-
+        //$this->call(VisitaEstadosSeeder::class);
     }
 }
